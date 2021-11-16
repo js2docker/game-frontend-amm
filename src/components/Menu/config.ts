afe8b7a866557d517445e49752844af16e05e575
js2docker <44265853+js2docker@users.noreply.github.com>
2021-11-16 01:14:37 -0500

    upload files

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,25 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://www.goosedefi.com/'
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap'
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool'
+      }
+    ]
+ ]
+
+export default config
